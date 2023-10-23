e96e50658a19b38b618a57ffc9806a98c60fe0d9
Isaac Onyemaechi <Amaechiisaac450@gmail.com>
2023-10-23 03:04:32 +0100

    fixed email script errors

--- email-alert.php
+++ email-alert.php
@@ -1,8 +1,4 @@
 <?php
-session_start();
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 $reference = $_SESSION['reference'];
 $name = $_SESSION['name'];
 $orderCR = $_SESSION['order_number'];
